2b039f489864253c806f5b9ddea4bf04897bbd66
Marcus Gama <marcusmgama@gmail.com>
2022-08-23 16:29:13 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using Course.Entities;
+using Course.Services;
+using System.Globalization;
+
+Console.WriteLine("Enter contract data");
+Console.Write("Number: ");
+int contractNumber = int.Parse(Console.ReadLine());
+Console.Write("Date dd/MM/yyyy: ");
+DateTime contractDate = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+Console.Write("Contract value: ");
+double contractValue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Enter number of installments: ");
+int months = int.Parse(Console.ReadLine());
+
+Contract contract = new Contract(contractNumber, contractDate, contractValue);
+
+ContractService contractService = new ContractService(new PaypalService());
+contractService.ProcessContract(contract, months);
+
+Console.WriteLine("Installments:");
+foreach(Installment installment in contract.Installments)
+{
+    Console.WriteLine(installment);
+}
+
+
+
